19706155620a054866d36bec2186984b8af7de44
Fabio Pagoti <fabiopagoti@gmail.com>
2015-03-26 15:20:15 -0300

    confirmation

---
+++ view/confirmation.view.js
@@ -0,0 +1,29 @@
+sap.ui.jsview("view.confirmation",
+{
+
+    /** Specifies the Controller belonging to this View. 
+     * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+     * @memberOf view.confirmation
+     */
+    getControllerName: function()
+    {
+        return "view.confirmation";
+    },
+
+    /** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+     * Since the Controller is given to this method, its event handlers can be attached right away.
+     * @memberOf view.confirmation
+     */
+    createContent: function(oController)
+    {
+
+        var page = new sap.m.Page(
+        {
+            title: "Solicitação de Exame - Confirmação",
+            content: []
+        });
+
+        return page;
+    }
+
+});
