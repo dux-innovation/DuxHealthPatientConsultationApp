746b030549d300d2a3a50f453ffdd0a1746ec7ef
Fabio Pagoti <fabiopagoti@gmail.com>
2015-03-26 13:46:08 -0300

    dates view added

--- view/dates.view.js
+++ view/dates.view.js
@@ -1,16 +1,16 @@
-sap.ui.jsview("view.units", {
+sap.ui.jsview("view.dates", {
 
     /** Specifies the Controller belonging to this View. 
      * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-     * @memberOf view.units
+     * @memberOf view.dates
      */
     getControllerName: function() {
-        return "view.units";
+        return "view.dates";
     },
 
     /** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
      * Since the Controller is given to this method, its event handlers can be attached right away.
-     * @memberOf view.units
+     * @memberOf view.dates
      */
     createContent: function(oController) {
         var table = new sap.m.Table({
