8fd17cbe06ae7e42c566994cbd488c97c6da6f03
Fabio Pagoti <fabiopagoti@gmail.com>
2015-03-26 12:02:02 -0300

    Initial version

---
+++ view/units.view.js
@@ -0,0 +1,49 @@
+sap.ui.jsview("view.units", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf view.units
+	*/ 
+	getControllerName : function() {
+		return "view.units";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf view.units
+	*/ 
+	createContent : function(oController) {
+		var table = new sap.m.Table({
+			items : {
+				path : "/especialidades/0/unidade",
+				template : new sap.m.ColumnListItem({
+					cells : [ new sap.m.Text({
+						text : "{unidades}",
+					}),
+					]
+				})
+			},
+			headerToolbar : new sap.m.Toolbar({
+				content : [], // sap.ui.core.Control
+			}),
+			infoToolbar : undefined,
+			columns : [ new sap.m.Column({
+				header : new sap.m.Text({
+					text : "Unidade",
+				})
+			}) ],
+			select : function(oEvent) {
+				var control = oEvent.getSource();
+			},
+
+		});
+
+		var page = new sap.m.Page({
+			title : "Solicitação de Exame - Unidade",
+			content : [ table ]
+		});
+
+		return page;
+	}
+
+});
